105954f49734ed9022a0e47a60997c947af74b9d
sun-jacobi <sun1011jacobi@gmail.com>
2022-11-24 00:19:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+### minimize the automaton
+
+```
+
+```
---
+++ Sources/automaton/main.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+print("number of states: ")
+let num_of_state = Int(readLine(strippingNewline: true)!)!
+
+var states : [Character] = Array(repeating: "A", count: num_of_state)
+
+
+print("size of alphabet : ")
+
+
+let size_of_alpha = Int(readLine(strippingNewline: true)!)!
+
+
+print("input the accept states (splitted by whitespace)")
+
+let accept_states = readLine()!.components(separatedBy: " ").map{ (a: String)->(Int) in
+    return Int(a)!
+}
+
+for state in accept_states {
+    states[state] = "B";
+}
+
+
+var table = 
+Array(repeating: Array(repeating: 0, count: size_of_alpha), count: num_of_state)
+
+print("input the automaton")
+for i in 0..<num_of_state {
+    let next_states = readLine()!.components(separatedBy: " ").map{ (a: String)->(Int) in
+    return Int(a)!
+    }    
+    assert(next_states.count == size_of_alpha)
+    for j in 0..<size_of_alpha {
+        table[i][j] = next_states[j]
+    }
+}
+
+
+while(true) {
+    var cnt = 0
+    var used_str = [String : Int]()
+    var str = ""
+    var next_states : [Character] = Array(repeating: "A", count: num_of_state)
+    for (state, to) in table.enumerated() {
+        for next_state in to {
+            str.append(states[next_state])
+        }
+        if let used_state = used_str[str]  {
+            next_states[state] = next_states[used_state]
+        } else {
+            used_str[str] = state
+            next_states[state] = Character(UnicodeScalar(65 + cnt)!)
+            cnt += 1
+        } 
+    }
+    if states.elementsEqual(next_states) {
+        print("answer is: ")
+        print(String(next_states))
+        break    
+    } else {
+        for (index, symbol) in next_states.enumerated() {
+           states[index] = symbol
+        }
+    }  
+}
+
+
+
+
